f9f42b2571abf072dba46335c8eb47c1efcf53d6
Soren Macbeth <soren@dopeness.org>
2016-11-15 13:22:45 -0800

    init

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to lemongrab
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject yieldbot/lemongrab "0.1.0-SNAPSHOT"
+  :description "Kryo for Clojure"
+  :url "https://github.com/yieldbot/lemongrab"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]])
