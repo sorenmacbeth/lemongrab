918d56edc15e43d91bbb4f36e48bb62360ef183b
Vebjorn Ljosa <vljosa@yieldbot.com>
2016-11-17 17:52:44 -0500

    Version 0.1.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject yieldbot/lemongrab "0.1.0"
+(defproject yieldbot/lemongrab "0.1.1-SNAPSHOT"
   :description "Kryo for Clojure"
   :url "https://github.com/yieldbot/lemongrab"
   :license {:name "Eclipse Public License"
